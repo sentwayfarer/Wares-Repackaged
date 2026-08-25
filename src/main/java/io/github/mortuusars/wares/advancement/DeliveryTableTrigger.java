e859de19e944b83f6a6310acf1fb56cf1ec7ca06
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-08-25 16:25:24 +0100

    Feel the weight of what we owe
    Fix DeliveryTableTrigger

--- src/main/java/io/github/mortuusars/wares/advancement/DeliveryTableTrigger.java
+++ src/main/java/io/github/mortuusars/wares/advancement/DeliveryTableTrigger.java
@@ -1,55 +1,49 @@
 package io.github.mortuusars.wares.advancement;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.mortuusars.wares.block.entity.DeliveryTableBlockEntity;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.advancements.critereon.*;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import org.jetbrains.annotations.NotNull;
 
+import java.util.Optional;
+
+@MethodsReturnNonnullByDefault
 public class DeliveryTableTrigger extends SimpleCriterionTrigger<DeliveryTableTrigger.TriggerInstance> {
-    private final ResourceLocation id;
-
-    public DeliveryTableTrigger(ResourceLocation id) {
-        this.id = id;
-    }
-
-    @Override
-    public @NotNull ResourceLocation getId() {
-        return id;
-    }
-
-    @Override
-    protected DeliveryTableTrigger.@NotNull TriggerInstance createInstance(@NotNull JsonObject json,
-                                                                           @NotNull ContextAwarePredicate predicate,
-                                                                           @NotNull DeserializationContext conditionsParser) {
-        return new DeliveryTableTrigger.TriggerInstance(getId(), predicate,
-                AgreementPredicate.fromJson(json.get("agreement")),
-                NbtPredicate.fromJson(json.get("agreement_nbt")),
-                LocationPredicate.fromJson(json.get("location")));
-    }
+    public DeliveryTableTrigger() {}
 
     public void trigger(ServerPlayer player, DeliveryTableBlockEntity tableBlockEntity) {
-        this.trigger(player, triggerInstance -> triggerInstance.matches(player, tableBlockEntity));
+        this.trigger(player, triggerInstance -> triggerInstance.matches(tableBlockEntity));
     }
 
-    public static class TriggerInstance extends AbstractCriterionTriggerInstance
+    @Override
+    public Codec<TriggerInstance> codec() {
+        return TriggerInstance.CODEC;
+    }
+
+    public record TriggerInstance(
+            Optional<ContextAwarePredicate> player,
+            AgreementPredicate agreement,
+            NbtPredicate agreementNbt,
+            LocationPredicate location
+    )
+            implements SimpleCriterionTrigger.SimpleInstance
     {
-        private final AgreementPredicate agreement;
-        private final NbtPredicate agreementNbt;
-        private final LocationPredicate location;
+        public static final Codec<TriggerInstance> CODEC =
+                RecordCodecBuilder.create(instance ->
+                        instance.group(
+                                EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(TriggerInstance::player),
+                                AgreementPredicate.CODEC.fieldOf("agreement").forGetter(TriggerInstance::agreement),
+                                NbtPredicate.CODEC.fieldOf("agreementNbt").forGetter(TriggerInstance::agreementNbt),
+                                LocationPredicate.CODEC.fieldOf("location").forGetter(TriggerInstance::location)
+                        ).apply(instance, TriggerInstance::new)
 
-        public TriggerInstance(ResourceLocation id, ContextAwarePredicate predicate, AgreementPredicate agreementPredicate,
-                               NbtPredicate agreementNbtPredicate, LocationPredicate locationPredicate) {
-            super(id, predicate);
-            agreement = agreementPredicate;
-            agreementNbt = agreementNbtPredicate;
-            location = locationPredicate;
-        }
+        );
 
-        public boolean matches(ServerPlayer player, DeliveryTableBlockEntity tableBlockEntity) {
+        public boolean matches(DeliveryTableBlockEntity tableBlockEntity) {
             if (!(tableBlockEntity.getLevel() instanceof ServerLevel serverLevel))
                 return false;
 
@@ -59,16 +53,8 @@ public class DeliveryTableTrigger extends SimpleCriterionTrigger<DeliveryTableTr
             double z = pos.getZ();
 
             return agreement.matches(tableBlockEntity.getAgreement())
-                    && agreementNbt.matches(tableBlockEntity.getAgreementItem().getTag())
+                    && agreementNbt.matches(tableBlockEntity.getAgreementItem())
                     && location.matches(serverLevel, x, y, z);
-        }
-
-        public @NotNull JsonObject serializeToJson(@NotNull SerializationContext pConditions) {
-            JsonObject jsonobject = super.serializeToJson(pConditions);
-            jsonobject.add("agreement", this.agreement.serializeToJson());
-            jsonobject.add("agreement_nbt", this.agreementNbt.serializeToJson());
-            jsonobject.add("location", this.location.serializeToJson());
-            return jsonobject;
         }
     }
 }
